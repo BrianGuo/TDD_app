8104645e324400879edef7a343712d87f28b385d
Brian Guo <Brian@brians-macbook.home>
2016-06-09 13:01:31 -0500

    added functional tests for layout and styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -16,7 +16,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -55,27 +55,30 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 # The page updates again and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
 # The page updates again and now shows both items on her list
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 # Now a new user, Francis, comes along to the site
 
-## We use a new browser session to make sure that no information
-## of edith's is coming through from cookies etc
+# We use a new browser session to make sure that no information
+# of edith's is coming through from cookies etc
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-#Francis visits the home page. There is no sign of edith's list
+# Francis visits the home page. There is no sign of edith's list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
-# Francis starts a new list by entering a new item. He is lest interesting than Edith
+# Francis starts a new list by entering a new item. He is lest interesting
+# than Edith
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
@@ -96,4 +99,24 @@ class NewVisitorTest(LiveServerTestCase):
 
 
 # She visits that URL - her to-do list is still there.
+    def test_layout_and_styling(self):
+        # edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        # She notices the input box is slightly centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+# She starts a new list and sees that the input is nicely centered there too
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
