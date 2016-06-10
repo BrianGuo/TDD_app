4c7e294fbfa152c7a63c2da90fbf0ab168b9ced7
Brian Guo <Brian@brians-macbook.home>
2016-06-09 23:04:08 -0500

    refactoring to heroku

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+            super().setUpClass()
+            cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Edith has heard about a cool new onlie to - do app. She goes to check
         # out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
     # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -70,7 +85,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
 # Francis visits the home page. There is no sign of edith's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -99,7 +114,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 # She visits that URL - her to-do list is still there.
     def test_layout_and_styling(self):
         # edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is slightly centered
